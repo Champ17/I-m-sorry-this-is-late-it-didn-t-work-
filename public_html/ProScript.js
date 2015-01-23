497440ae5a471990d0d4f1b211e877115c345628
Miguel Banuelos <mistergengar@gmail.com>
2015-01-23 07:25:52 -0800

    I'm sorry but Github wasn't working yesterday SORRRY!!

---
+++ public_html/ProScript.js
@@ -0,0 +1,38 @@
+$("document").ready(function() {
+    $("#Project").css("background-image", "url(http://images4.fanpop.com/image/photos/18200000/Night-Guard-teddybear64-18220939-1280-800.jpg)");
+    $("#Project").css("background-size", "cover");
+
+    $(".text-center").css("color", "white");
+
+    $(".container").css("border-color", "purple");
+    $(".container").css(" border-width", "5px");
+    $(".container").css("padding", "50px");
+    $(".container").css("border-style", " groove");
+
+    $(".col-md-6").css("border-color", "purple");
+    $(".col-md-6").css(" border-width", "5px");
+    $(".col-md-6").css("padding", "50px");
+    $(".col-md-6").css("border-style", " groove");
+
+    $("#Psych").css("color", "white");
+
+    $("#NO").css("color", "white");
+    $("#jac").css("color", "white");
+    $("#lass").css("color", "white");
+    $("#Ted").css("color", "white");
+    $("#nan").css("color", "white");
+    $("#lil").css("color", "white");
+    $("#rob").css("color", "white");
+    $("#cat").css("color", "white");
+    $("#sen").css("color", "white");
+
+    $("#ZA").css("color", "black");
+
+    $("#GT").css("color", "white");
+    $("#GT").css("text-align", "center");
+
+    $("#DeB").accordion({header: "h3"});
+    $("#DeB").css("opacity", "1.0");
+
+
+});
